584335cdce8ee0b332ed107aa0002d3e42d7151a
Dastan Tulokulov <dastan.tulokulov.kg@mail.ru>
2019-09-18 16:49:05 +0600

    а так

--- src/Main.java
+++ src/Main.java
@@ -6,10 +6,15 @@ public class Main {
 
     public static void main(String[] args) {
         HashMap<String, String[]> dictionary = new HashMap<>();
+        HashMap<String, String[]> bigDictionary = new HashMap<>();
         dictionary.put("big", new String[]{"bigger", "the biggest", "huge", "giant"});
         dictionary.put("home", new String[]{"house", "flat", "apartment"});
         dictionary.put("beautiful", new String[]{"pretty", "cute", "handsome", "attractive"});
         dictionary.put("person", new String[]{"people", "individual", "man", "woman"});
+
+        Set<String> keys = dictionary.keySet();
+        Collection<String[]> values = dictionary.values();
+        Iterator<String> iterator = keys.iterator();
 
         System.out.println("Please input your word: ");
         Scanner scanner = new Scanner(System.in);
@@ -21,33 +26,30 @@ public class Main {
             if (synonyms != null) {
                 int randomNumber = random.nextInt(synonyms.length);
                 System.out.println(synonyms[randomNumber] + " ");
-            } else {
+            } else if (sentences == words[i])
+            while (iterator.hasNext()) {
+                String key = iterator.next();
+                String[] value = dictionary.get(key);
+                bigDictionary.put(key, value);
+                for (int k = 0; k < value.length; k++) {
+                    String newKey = value[k];
+                    ArrayList<String> newDictionary = new ArrayList<>(value.length);
+                    newDictionary.addAll(Arrays.asList(value));
+                    newDictionary.remove(newKey);
+                    newDictionary.add(key);
+                    String[] newValues = new String[newDictionary.size()];
+                    newValues = newDictionary.toArray(newValues);
+                    System.out.println(newKey + " - " + Arrays.toString(newValues));
+                    bigDictionary.put(newKey, newValues);
+                }
+            }
+            else{
                 System.out.println("Excuse me, i did not get your input");
             }
 
         }
 
-        HashMap<String, String[]> bigDictionary = new HashMap<>();
 
-        Set<String> keys = dictionary.keySet();
-        Collection<String[]> values = dictionary.values();
-        Iterator<String> iterator = keys.iterator();
-        while (iterator.hasNext()){
-            String key = iterator.next();
-            String[] value = dictionary.get(key);
-            bigDictionary.put(key, value);
-            for (int k = 0; k < value.length; k++) {
-                String newKey = value[k];
-                ArrayList<String> newDictionary = new ArrayList<>(value.length);
-                newDictionary.addAll(Arrays.asList(value));
-                newDictionary.remove(newKey);
-                newDictionary.add(key);
-                String[] newValues = new String[newDictionary.size()];
-                newValues = newDictionary.toArray(newValues);
-                System.out.println(newKey + " - " + Arrays.toString(newValues));
-                bigDictionary.put(newKey, newValues);
-            }
-            }
 
             /*if (bigDictionary = scanner) {
                 int randomNumber = random.nextInt(words.length);
